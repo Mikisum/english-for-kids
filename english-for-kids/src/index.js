a5c53934dde9cc17ff11ac2938a7b6ff002faa9a
Viktoryia Kiyanka <mikisum@mail.ru>
2020-04-21 00:23:41 +0300

    feat: add generating page layout

--- english-for-kids/src/index.js
+++ english-for-kids/src/index.js
@@ -2,6 +2,161 @@
 import "./style.css";
 import cards from './cards.js';
 
+
+class MenuCard {
+  constructor(categoryName) {
+    let imageName = cards[categoryName][1].image;
+
+    this.cardContainer = document.createElement('div');
+    this.cardContainer.className = 'p-4 col-sm-6 col-md-4 col-lg-3';
+
+    this.card = document.createElement('div');
+    this.card.className = 'card';
+    this.cardContainer.append(this.card);
+
+    let imageContainer = document.createElement('div');
+    imageContainer.className = 'image';
+    this.card.append(imageContainer);
+
+    let image = document.createElement('img');
+    image.className = 'rounded-circle img-thumbnail';
+    image.setAttribute('src', '../' + imageName);
+    imageContainer.append(image);
+
+    let cardLink = document.createElement('a');
+    cardLink.className = 'stretched-link';
+    cardLink.setAttribute('href', '#');
+    this.card.append(cardLink);
+
+    let cardTextContainer = document.createElement('div');
+    cardTextContainer.className = 'card-inner';
+    cardLink.append(cardTextContainer);
+
+    this.cardText = document.createElement('h2');
+    this.cardText.innerText = categoryName;
+    cardTextContainer.append(this.cardText);
+  }
+
+  getHtmlElement() {
+    return this.cardContainer;
+  }
+
+};
+
+
+class CategoryCard {
+  constructor({word, translation, image, audioSrc}) {
+
+    this.cardContainer = document.createElement('div');
+    this.cardContainer.className = 'p-3';
+    
+    this.card = document.createElement('div');
+    this.card.className = 'card card-active';
+    this.cardContainer.append(this.card);
+
+    this.cardFront = document.createElement('div');
+    this.cardFront.className = 'front';
+    this.card.append(this.cardFront);
+
+    let imageContainerFront = document.createElement('div');
+    imageContainerFront.className = 'image';
+    this.cardFront.append(imageContainerFront);
+
+    this.imgFront = document.createElement('img');
+    this.imgFront.setAttribute('src', '../' + image);
+    imageContainerFront.append(this.imgFront);
+
+    let description = document.createElement('div');
+    description.setAttribute('id', 'description');
+    description.className = 'd-flex justify-content-end px-2';
+    this.cardFront.append(description);
+
+    this.textEN = document.createElement('div');
+    this.textEN.className = 'card-inner';
+    description.append(this.textEN);
+
+    this.wordEN = document.createElement('h2');
+    this.wordEN.innerText = word;
+    this.textEN.append(this.wordEN);
+
+    this.iconContainer = document.createElement('div');
+    this.iconContainer.className = 'icon fa-2x color-light';
+    description.append(this.iconContainer);
+
+    let icon = document.createElement('i');
+    icon.className = 'fas fa-redo';
+    icon.setAttribute('data', 'flip');
+    this.iconContainer.append(icon);
+
+    let link = document.createElement('a');
+    link.className = 'stretched-link';
+    link.setAttribute('href', '#');
+    link.setAttribute('data', 'audioLink');
+    this.cardFront.append(link);
+
+    this.audio = document.createElement('audio');
+    this.audio.setAttribute('src', '../' + audioSrc);
+    link.append(this.audio);
+
+    /* Card back side */
+    this.cardBack = document.createElement('div');
+    this.cardBack.className = 'back';
+    this.card.append(this.cardBack);
+
+    let imageContainerBack = document.createElement('div');
+    imageContainerBack.className = 'image';
+    this.cardBack.append(imageContainerBack);
+
+    this.imgBack = document.createElement('img');
+    this.imgBack.setAttribute('src', '../' + image);
+    imageContainerBack.append(this.imgBack);
+
+    let descriptionBack= document.createElement('div');
+    descriptionBack.className = 'd-flex justify-content-center px-2';
+    this.cardBack.append(descriptionBack);
+
+    let cardTextContainer = document.createElement('div');
+    cardTextContainer.className = 'card-inner';
+    descriptionBack.append(cardTextContainer);
+
+    this.wordRU = document.createElement('h2');
+    this.wordRU.innerText = translation;
+    cardTextContainer.append(this.wordRU);
+  }
+
+  getHtmlElement() {
+    return this.cardContainer;
+  }
+
+  update({word, translation, image, audioSrc}) {
+    this.card.classList.remove('card-inactive');
+    this.card.classList.add('card-active');
+    this.wordEN.innerText = word;
+    this.wordRU.innerText = translation;
+    this.imgFront.setAttribute('src', '../' + image);
+    this.imgBack.setAttribute('src', '../' + image);
+    this.audio.setAttribute('src', '../' + audioSrc);
+  }
+};
+
+
+let menuCards = [];
+let cardDeck = document.getElementById('mainPage');
+Object.keys(cards).forEach(element => {
+  let menuCard = new MenuCard(element);
+  cardDeck.append(menuCard.getHtmlElement());
+  menuCards.push(menuCard);
+});
+
+let categoryCards = [];
+let firstCategory = Object.keys(cards)[0];
+let category = document.getElementById('category-page');
+cards[firstCategory].forEach(element => {
+  let card = new CategoryCard(element)
+  category.append(card.getHtmlElement());
+  categoryCards.push(card);
+});
+
 const container = document.getElementById('pageContainer');
 const categoryPage = document.getElementById('category');
 
@@ -38,9 +193,8 @@ switcher.addEventListener('change', (event) => {
 function changeButton() {
   button.classList.remove('btn-lg');
   button.classList.add('btn-rounded');
-  button.setAttribute('id', 'repeat');
+  button.setAttribute('data', 'repeat');
   button.innerText = "";
-  icon.setAttribute('id', 'repeat');
   button.append(icon);
   buttonPressed = true;
 }
@@ -48,11 +202,11 @@ function changeButton() {
 function restoreButton() {
   button.classList.remove('btn-rounded');
   button.classList.add('btn-lg');
-  button.removeAttribute('id');
+  button.removeAttribute('data', 'repeat');
+  button.setAttribute('data', 'startGame')
+  icon.remove(icon);
   button.innerText = "Start game";
   icon.removeAttribute('id');
-  // if (button.lastChild)
-  //   button.removeChild(button.lastChild);
   buttonPressed = false;
 }
 
@@ -92,13 +246,13 @@ let buttonPressed = false;
 let currentCard;
 
 categoryPage.addEventListener('click', (event) => {
-  if (event.target.getAttribute('id') === 'flip') {
+  if (event.target.getAttribute('data') === 'flip') {
     event.target.closest('.card').classList.add("flipped");
   }
-  else if (event.target.classList.contains('stretched-link') && !gameMode) {
+  else if ((event.target.getAttribute('data') === 'audioLink') && !gameMode) {
     event.target.firstElementChild.play();
   }
-  else if (event.target.classList.contains('stretched-link') && gameMode && buttonPressed && !ignore) {
+  else if ((event.target.getAttribute('data') === 'audioLink') && gameMode && buttonPressed && !ignore) {
     
     if (event.target.closest('.card').innerText === currentCard.innerText) {
       ignore = true;
@@ -212,16 +366,22 @@ function updateCards(categoryName) {
     rating.removeChild(rating.lastChild);
   }
 
-  for (let i = 0; i < htmlCards.length; i++) {
-    htmlCards[i].classList.remove('card-inactive');
-    htmlCards[i].classList.add('card-active');
-    htmlCards[i].getElementsByTagName('img').forEach(element => {
-      element.src = '../' + cards[categoryName][i].image;
-    });
-    htmlCards[i].getElementsByTagName('h2')[0].innerText = cards[categoryName][i].word;
-    htmlCards[i].getElementsByClassName('translation')[0].innerText = cards[categoryName][i].translation;
-    htmlCards[i].getElementsByTagName('audio')[0].src = '../' + cards[categoryName][i].audioSrc;
-  }
+  let index = 0;
+  cards[categoryName].forEach(element => {
+    categoryCards[index].update(element);
+    index++;
+  });
+
+  // for (let i = 0; i < htmlCards.length; i++) {
+  //   htmlCards[i].classList.remove('card-inactive');
+  //   htmlCards[i].classList.add('card-active');
+  //   htmlCards[i].getElementsByTagName('img').forEach(element => {
+  //     element.src = '../' + cards[categoryName][i].image;
+  //   });
+  //   htmlCards[i].getElementsByTagName('h2')[0].innerText = cards[categoryName][i].word;
+  //   htmlCards[i].getElementsByClassName('translation')[0].innerText = cards[categoryName][i].translation;
+  //   htmlCards[i].getElementsByTagName('audio')[0].src = '../' + cards[categoryName][i].audioSrc;
+  // }
   restoreButton();
 };
 
