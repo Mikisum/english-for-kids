518fb5773a27231da607d6469aa7033a62f434d5
Viktoryia Kiyanka <mikisum@mail.ru>
2020-04-24 15:35:13 +0300

    refactor: fix eslint erors

--- english-for-kids/src/index.js
+++ english-for-kids/src/index.js
@@ -3,7 +3,6 @@ import cards from './cards';
 import MenuCard from './MenuCard';
 import CategoryCard from './CategoryCard';
 
-
 const clickTargets = {
   flip: 'flip',
   cardLink: 'cardLink',
@@ -40,43 +39,23 @@ function hideSidebar() {
   const sidebar = document.getElementById('sidebar');
   sidebar.classList.remove('show');
 }
-// sideBar click 
-document.querySelector('.nav').addEventListener('click', (event) => {
-    navLinks.forEach(a => {
-        a.classList.remove('active');
-    })
-    event.target.classList.add('active');
-    if (event.target.classList.contains('menu')) {
-        categoryPage.style.display = 'none';
-        container.style.display = 'block';
-        hideSidebar();
-    } else {
-        container.style.display = 'none';
-        categoryPage.style.display = 'block';
-        hideSidebar();
 
-    }
-    const getNameCategory = event.target.innerText;
-    updateCards(getNameCategory);
-});
+const rating = document.getElementById('rating');
+function clearRaiting() {
+  while (rating.firstChild) {
+    rating.removeChild(rating.lastChild);
+  }
+};
 
-// change Play/Train mode
-let gameMode;
-const switcher = document.querySelector('.tgl');
-switcher.addEventListener('change', (event) => {
-  gameMode = event.target.checked;
-  checkGameMode();
-});
-
-const button = document.querySelector('.btn');
 const iconRepeat =  document.createElement('i');
-
+const button = document.querySelector('.btn');
 function createIconRepeat() {
   iconRepeat.className = 'fas fa-3x fa-redo icon-repeat';
   iconRepeat.setAttribute('data', 'repeat');
   button.append(iconRepeat);
 }
 
+let buttonPressed = false;
 function changeButton() {
   button.classList.remove('btn-lg');
   button.classList.add('btn-rounded');
@@ -96,48 +75,133 @@ function restoreButton() {
   buttonPressed = false;
 }
 
-// const htmlCards = categoryPage.getElementsByClassName('card');
-function checkGameMode() {
-   if (gameMode) {
-    menuCards.forEach(element => {
-      element.getHtmlElement().classList.add('page-game');
-    });
+function updateCards(categoryName) {
+  clearRaiting();
+  let index = 0;
+  cards[categoryName].forEach(element => {
+    categoryCards[index].update(element);
+    index += 1;
+  });
+  restoreButton();
+};
+// sideBar click 
+document.querySelector('.nav').addEventListener('click', (event) => {
+    navLinks.forEach(a => {
+        a.classList.remove('active');
+    })
+    event.target.classList.add('active');
+    if (event.target.classList.contains('menu')) {
+        categoryPage.style.display = 'none';
+        container.style.display = 'block';
+        hideSidebar();
+    } else {
+        container.style.display = 'none';
+        categoryPage.style.display = 'block';
+        hideSidebar();
 
-    categoryCards.forEach(element => {
-      element.getHtmlDescription().classList.add('none');
-      element.getHtmlImage().classList.add('card-cover');
-    });
-    button.classList.add('btn-active');
-    document.getElementById('nav').classList.add('page-game');
-  }
-  else {
-    const mainCads = container.querySelectorAll('.card');
-    mainCads.forEach(element => {
-      element.classList.remove('page-game');
-    });
-    categoryCards.forEach(element => {
-      element.getHtmlDescription().classList.remove('none');
-      element.getHtmlImage().classList.remove('card-cover');
-      element.getHtmlElement().classList.remove('card-inactive');
-      
-    });
-    button.classList.remove('btn-active');
-    document.getElementById('nav').classList.remove('page-game');
-    clearRaiting();
-  }
+    }
+    const getNameCategory = event.target.innerText;
+    updateCards(getNameCategory);
+});
+let gameMode;
+function checkGameMode() {
+  if (gameMode) {
+   menuCards.forEach(element => {
+     element.getHtmlElement().classList.add('page-game');
+   });
+
+   categoryCards.forEach(element => {
+     element.getHtmlDescription().classList.add('none');
+     element.getHtmlImage().classList.add('card-cover');
+   });
+   button.classList.add('btn-active');
+   document.getElementById('nav').classList.add('page-game');
+ }
+ else {
+   const mainCads = container.querySelectorAll('.card');
+   mainCads.forEach(element => {
+     element.classList.remove('page-game');
+   });
+   categoryCards.forEach(element => {
+     element.getHtmlDescription().classList.remove('none');
+     element.getHtmlImage().classList.remove('card-cover');
+     element.getHtmlElement().classList.remove('card-inactive');
+     
+   });
+   button.classList.remove('btn-active');
+   document.getElementById('nav').classList.remove('page-game');
+   clearRaiting();
+ }
 }
+// change Play/Train mode
+const switcher = document.querySelector('.tgl');
+switcher.addEventListener('change', (event) => {
+  gameMode = event.target.checked;
+  checkGameMode();
+});
 // flipping a card when clicking on the icon
 const cardActive = categoryPage.getElementsByClassName('card-active');
-const rating = document.getElementById('rating');
 let ignore = false;
-
-let buttonPressed = false;
 let currentCard;
 
 function createIconStar(className) {
   const star = document.createElement('i');
   star.className = `${className} fa-2x fa-star`;
   rating.append(star);
+}
+
+function getRandomCard() {
+  const x = Math.floor(Math.random() * cardActive.length);
+  currentCard = cardActive[x];
+}
+
+function showFail() {
+  document.getElementById('wrapper').classList.add('hidden');
+  const errorText = document.createElement('h2');
+  errorText.classList.add('result');
+  errorText.innerText = `${gameResult.erros} errors`;
+  document.body.append(errorText);
+  document.body.classList.add('failure');
+  new Audio('../audio/failure.mp3').play();
+}
+
+function showSuccess() {
+  document.body.classList.add('success');
+  new Audio('../audio/success.mp3').play();
+}
+
+function returnMain() {
+  if (!gameResult.status) {
+    document.body.classList.remove('failure');
+    const errorText = document.body.getElementsByClassName('result')[0];
+    errorText.remove();  
+    document.getElementById('wrapper').classList.remove('hidden');
+  }
+  else {
+    document.body.classList.remove('success');
+  }
+  categoryPage.style.display = 'none';
+  container.style.display = 'block';
+  switcher.checked = false;
+  gameMode = switcher.checked;
+  checkGameMode();
+}
+
+function finishGame() {
+  gameResult.erros = rating.querySelectorAll('.far').length;
+  gameResult.status = (gameResult.erros === 0);
+  if (!gameResult.status) {
+    showFail();
+  }
+  else {
+    showSuccess();
+  }
+  setTimeout(returnMain, 3000);
+}
+
+function playAudio () {
+  currentCard.getElementsByTagName('audio')[0].play();
+  ignore = false;
 }
 
 categoryPage.addEventListener('click', (event) => {
@@ -161,7 +225,7 @@ categoryPage.addEventListener('click', (event) => {
       clickedCard.classList.remove('card-active');
       clickedCard.classList.add('card-inactive');
       audio.play();
-      randomCard();
+      getRandomCard();
       if (!currentCard) {
         finishGame();
       }
@@ -175,71 +239,16 @@ categoryPage.addEventListener('click', (event) => {
       audio.play();
     }
   }
-
   else if (clickedTarget === clickTargets.repeat) {
     currentCard.getElementsByTagName('audio')[0].play();
   }
   else if (clickedTarget === clickTargets.startGame) {
     changeButton();
-    randomCard();
+    getRandomCard();
     setTimeout(playAudio, 1000);
   }
   hideSidebar();
 });
-
-function playAudio () {
-  currentCard.getElementsByTagName('audio')[0].play();
-  ignore = false;
-}
-
-function returnMain() {
-  if (!gameResult.status) {
-    document.body.classList.remove('failure');
-    const errorText = document.body.getElementsByClassName('result')[0];
-    errorText.remove();  
-    document.getElementById('wrapper').classList.remove('hidden');
-  }
-  else {
-    document.body.classList.remove('success');
-  }
-  categoryPage.style.display = 'none';
-  container.style.display = 'block';
-  switcher.checked = false;
-  gameMode = switcher.checked;
-  checkGameMode();
-}
-
-function showFail() {
-  document.getElementById('wrapper').classList.add('hidden');
-  const errorText = document.createElement('h2');
-  errorText.classList.add('result');
-  errorText.innerText = `${gameResult.erros} errors`;
-  document.body.append(errorText);
-  document.body.classList.add('failure');
-  new Audio('../audio/failure.mp3').play();
-}
-
-function showSuccess() {
-  document.body.classList.add('success');
-  new Audio('../audio/success.mp3').play();
-}
-function finishGame() {
-  gameResult.erros = rating.querySelectorAll('.far').length;
-  gameResult.status = (gameResult.erros === 0);
-  if (!gameResult.status) {
-    showFail();
-  }
-  else {
-    showSuccess();
-  }
-
-  setTimeout(returnMain, 3000);
-}
-
-function randomCard() {
-  const x = Math.floor(Math.random() * cardActive.length);
-  currentCard = cardActive[x];
-}
 // flip back a card on mouseout
 categoryPage.addEventListener('mouseout', (event) => {
   if (!event.target || !event.relatedTarget) return;
@@ -266,26 +275,3 @@ container.addEventListener('click', (event) => {
     }
     hideSidebar();
 });
-
-function clearRaiting() {
-  while (rating.firstChild) {
-    rating.removeChild(rating.lastChild);
-  }
-};
-function updateCards(categoryName) {
-  clearRaiting();
-  let index = 0;
-  cards[categoryName].forEach(element => {
-    categoryCards[index].update(element);
-    index += 1;
-  });
-  restoreButton();
-};
-
-
-
-
-
-
-
-
